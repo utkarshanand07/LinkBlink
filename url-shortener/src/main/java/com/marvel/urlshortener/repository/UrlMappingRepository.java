75cb6e1c719ea0fcc639f9322cd538eb69e6b76a
UtkarshAnand07 <utkarshanand7193@gmail.com>
2025-11-07 16:19:28 +0530

    initial commit

---
+++ url-shortener/src/main/java/com/marvel/urlshortener/repository/UrlMappingRepository.java
@@ -0,0 +1,14 @@
+package com.marvel.urlshortener.repository;
+
+import com.marvel.urlshortener.models.UrlMapping;
+import com.marvel.urlshortener.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+    UrlMapping findByShortUrl(String shortUrl);
+    List<UrlMapping> findByUser(User user);
+}
