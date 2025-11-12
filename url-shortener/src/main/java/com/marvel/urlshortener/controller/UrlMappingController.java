8f26369d2a468d08ae5feba5ce80de174f8598d7
UtkarshAnand07 <utkarshanand7193@gmail.com>
2025-11-12 20:20:18 +0530

    Added Url shortening

---
+++ url-shortener/src/main/java/com/marvel/urlshortener/controller/UrlMappingController.java
@@ -0,0 +1,41 @@
+package com.marvel.urlshortener.controller;
+
+import com.marvel.urlshortener.dtos.UrlMappingDTO;
+import com.marvel.urlshortener.models.User;
+import com.marvel.urlshortener.service.UrlMappingService;
+import com.marvel.urlshortener.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/urls")
+@AllArgsConstructor
+public class UrlMappingController {
+    private UrlMappingService urlMappingService;
+    private UserService userService;
+
+    @PostMapping("/shorten")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<UrlMappingDTO> createShortUrl(@RequestBody Map<String, String> request,
+                                                        Principal principal){
+        String originalUrl = request.get("originalUrl");
+        User user = userService.findByUsername(principal.getName());
+        UrlMappingDTO urlMappingDTO = urlMappingService.createShortUrl(originalUrl, user);
+        return ResponseEntity.ok(urlMappingDTO);
+    }
+
+
+    @GetMapping("/myurls")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<List<UrlMappingDTO>> getUserUrls(Principal principal){
+        User user = userService.findByUsername(principal.getName());
+        List<UrlMappingDTO> urls = urlMappingService.getUrlsByUser(user);
+        return ResponseEntity.ok(urls);
+    }
+}
---
+++ url-shortener/src/main/java/com/marvel/urlshortener/service/UrlMappingService.java
@@ -0,0 +1,68 @@
+package com.marvel.urlshortener.service;
+
+import com.marvel.urlshortener.dtos.UrlMappingDTO;
+import com.marvel.urlshortener.models.UrlMapping;
+import com.marvel.urlshortener.models.User;
+import com.marvel.urlshortener.repository.UrlMappingRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class UrlMappingService {
+    private UrlMappingRepository urlMappingRepository;
+
+    public UrlMappingDTO createShortUrl(String originalUrl, User user) {
+        String shortUrl = generateShortUrl();
+        UrlMapping urlMapping = new UrlMapping();
+        urlMapping.setOriginalUrl(originalUrl);
+        urlMapping.setShortUrl(shortUrl);
+        urlMapping.setUser(user);
+        urlMapping.setCreatedDate(LocalDateTime.now());
+        UrlMapping savedUrlMapping = urlMappingRepository.save(urlMapping);
+        return convertToDto(savedUrlMapping);
+    }
+
+    private UrlMappingDTO convertToDto(UrlMapping urlMapping){
+        UrlMappingDTO urlMappingDTO = new UrlMappingDTO();
+        urlMappingDTO.setId(urlMapping.getId());
+        urlMappingDTO.setOriginalUrl(urlMapping.getOriginalUrl());
+        urlMappingDTO.setShortUrl(urlMapping.getShortUrl());
+        urlMappingDTO.setClickCount(urlMapping.getClickCount());
+        urlMappingDTO.setCreatedDate(urlMapping.getCreatedDate());
+        urlMappingDTO.setUsername(urlMapping.getUser().getUsername());
+        return urlMappingDTO;
+    }
+
+    private String generateShortUrl() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        Random random = new Random();
+        StringBuilder shortUrl = new StringBuilder(8);
+
+        for (int i = 0; i < 8; i++) {
+            shortUrl.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return shortUrl.toString();
+    }
+
+    public List<UrlMappingDTO> getUrlsByUser(User user) {
+        return urlMappingRepository.findByUser(user).stream()
+                .map(this::convertToDto)
+                .toList();
+    }
+
+    public UrlMapping getOriginalUrl(String shortUrl) {
+        UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        if (urlMapping != null) {
+            urlMapping.setClickCount(urlMapping.getClickCount() + 1);
+            urlMappingRepository.save(urlMapping);
+        }
+
+        return urlMapping;
+    }
+}
