386e3d5b243c9df2dfccfee36e27662e5894d7fe
UtkarshAnand07 <utkarshanand7193@gmail.com>
2025-11-12 20:56:38 +0530

    Added Url Redirect

---
+++ url-shortener/src/main/java/com/marvel/urlshortener/controller/RedirectController.java
@@ -0,0 +1,28 @@
+package com.marvel.urlshortener.controller;
+
+import com.marvel.urlshortener.models.UrlMapping;
+import com.marvel.urlshortener.service.UrlMappingService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class RedirectController {
+    private UrlMappingService urlMappingService;
+
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortUrl){
+        UrlMapping urlMapping = urlMappingService.getOriginalUrl(shortUrl);
+        if (urlMapping != null) {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("Location", urlMapping.getOriginalUrl());
+            return ResponseEntity.status(302).headers(httpHeaders).build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
