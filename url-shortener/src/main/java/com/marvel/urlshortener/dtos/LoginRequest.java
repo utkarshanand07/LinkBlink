eaeacc5132d8a7b6f7eb4c2a21f7e1aa934deaf6
UtkarshAnand07 <utkarshanand7193@gmail.com>
2025-11-12 19:42:57 +0530

    added auth

---
+++ url-shortener/src/main/java/com/marvel/urlshortener/dtos/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.marvel.urlshortener.dtos;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String username;
+    private String password;
+}
---
+++ url-shortener/src/main/java/com/marvel/urlshortener/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.marvel.urlshortener.service;
+
+import com.marvel.urlshortener.models.User;
+import com.marvel.urlshortener.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: "+ username));
+        return UserDetailsImpl.build(user);
+    }
+}
