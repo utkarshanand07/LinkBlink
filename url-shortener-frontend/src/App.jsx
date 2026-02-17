968a2865856808bfe378a3d26451e61f5deb2a05
UtkarshAnand07 <utkarshanand7193@gmail.com>
2026-02-17 21:22:04 +0530

    added pages

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -5,16 +5,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import LandingPage from './components/LandingPage' 
 import AboutPage from './components/AboutPage'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
--- url-shortener-frontend/src/components/Footer.jsx
+++ url-shortener-frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-custom-gradient text-white py-8 z-40 relative">
+    <footer className="bg-slate-900 text-white py-8 z-40 relative">
       <div className="container mx-auto px-6 lg:px-14 flex flex-col lg:flex-row lg:justify-between items-center gap-4">
         <div className="text-center lg:text-left">
           <h2 className="text-3xl font-bold mb-2">LinkBlink</h2>
