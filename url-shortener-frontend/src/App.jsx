b89a420e806026be15af62709102787ef83bc725
UtkarshAnand07 <utkarshanand7193@gmail.com>
2026-02-17 22:17:58 +0530

    added auth

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom' 
@@ -7,6 +6,11 @@ import LandingPage from './components/LandingPage'
 import AboutPage from './components/AboutPage'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import RegisterPage from './components/RegisterPage'
+import LoginPage from './components/LoginPage'   
+import DashboardLayout from './Dashboard/DashboardLayout'
+
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
@@ -14,9 +18,13 @@ function App() {
     <>
       <BrowserRouter>
         <NavBar />
+        <Toaster position='bottom-center'/>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/Register" element={<RegisterPage />} />
+          <Route path="/Login" element={<LoginPage />} />
+          <Route path="/Dashboard" element={<DashboardLayout />} />
         </Routes>
         <Footer />
       </BrowserRouter>
